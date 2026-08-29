acbfe35bbe26c4d1c6e0daaea3d2167e3f7dc2b0
Rafael Teles Espindola <rafatespindola@gmail.com>
2026-08-29 12:11:37 -0300

    adicionado rabbitmq, mas ainda não publica nada

--- agendamento-service/src/main/java/br/com/fiap/agendamento/config/RabbitMQConfig.java
+++ agendamento-service/src/main/java/br/com/fiap/agendamento/config/RabbitMQConfig.java
@@ -1,4 +1,55 @@
 package br.com.fiap.agendamento.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
+
+    @Value("${lab.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${lab.rabbitmq.queue}")
+    private String queue;
+
+    @Value("${lab.rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public TopicExchange agendamentosExchange() {
+        return new TopicExchange(exchange);
+    }
+
+    // Fila e binding declarados aqui para o lab funcionar sem depender do consumer.
+    @Bean
+    public Queue agendamentosQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public Binding agendamentosBinding(Queue pedidosQueue, TopicExchange pedidosExchange) {
+        return BindingBuilder.bind(pedidosQueue).to(pedidosExchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
 }
